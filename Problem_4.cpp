1993d98838dd4ec2b4582e03bb88062d3f4d510f
leonardo@roffor.com.br <leonardo@roffor.com.br>
2017-09-26 21:11:31 -0300

    solution of problem 4

---
+++ Problem_4.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    int big = 0;
+    
+    for(int i = 901; i <= 999; i++) {
+        for(int j = 902; j <= 999; j ++) {
+            std::string normal = std::to_string(i*j);
+            std::string copy(normal);
+            std::reverse(copy.begin(), copy.end());
+            if( normal == copy ) {
+                big = i*j;
+            }
+            
+        }
+    }
+    std::cout << big << std::endl;
+    return 0;
+}
+
+
+
